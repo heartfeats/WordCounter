9b5ad42938f910df9e505d1e8e7309e9bee11886
jm <heartfeats@gmail.com>
2017-10-20 09:56:07 -0700

    add initial folders and files

---
+++ WordCounter/Startup.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WordCounter {
+    public class Startup {
+        public Startup (IHostingEnvironment env) {
+            var builder = new ConfigurationBuilder ().SetBasePath (env.ContentRootPath).AddEnvironmentVariables ();
+            Configuration = builder.Build ();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddMvc ();
+        }
+
+        public void Configure (IApplicationBuilder app) {
+            app.UseDeveloperExceptionPage ();
+            app.UseStaticFiles ();
+            app.UseMvc (routes => {
+                routes.MapRoute (
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
